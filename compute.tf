2c3721652ba3f98b8c5b2eed5d364d7bc4991422
Anil <anilnotanneal@gmail.com>
2025-12-11 13:48:13 -0500

    Updated compute.tf to declare aws_ami and ubuntu resources.

--- compute.tf
+++ compute.tf
@@ -1,3 +1,19 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  owners = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "vaultwarden" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
